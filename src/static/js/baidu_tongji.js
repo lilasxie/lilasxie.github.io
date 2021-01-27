110dd74c3ead499aa9774a22164b74746c1300c1
linzhixian <linzhixian@xunlei.com>
2021-01-27 21:52:08 +0800

    add baidu tongji & fix submodule

---
+++ src/static/js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bcee3b6d24a62df7c1e0c03b14c950a9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
