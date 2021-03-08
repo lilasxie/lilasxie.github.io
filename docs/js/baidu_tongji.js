8638fcce46d726cff061937ebefb0db2c54fd522
lilasxie <thanklilas@163.com>
2021-03-08 23:57:21 +0800

    1. update statistics; 2. change avatar image

--- docs/js/baidu_tongji.js
+++ docs/js/baidu_tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?bcee3b6d24a62df7c1e0c03b14c950a9";
+  hm.src = "https://hm.baidu.com/hm.js?dba902e1a52cc8954c13c2f3f0eba76d";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
